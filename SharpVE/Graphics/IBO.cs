15bfec09d98021abca4bd88a0f4df12600f37559
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2023-12-09 19:10:41 +1100

    IBO

---
+++ SharpVE/Graphics/IBO.cs
@@ -0,0 +1,66 @@
+﻿using Silk.NET.OpenGL;
+
+namespace SharpVE.Graphics
+{
+    public class IBO
+    {
+        public uint ID { get; private set; }
+        public int Length => Indices.Length;
+        private uint[] Indices;
+
+        protected readonly GL GraphicsInstance;
+
+        public IBO(GL gl)
+        {
+            ID = gl.GenBuffer();
+            GraphicsInstance = gl;
+            Indices = new uint[0];
+        }
+
+        /// <summary>
+        /// Sets the data.
+        /// </summary>
+        /// <param name="data"></param>
+        public void SetData(List<uint> data)
+        {
+            Indices = data.ToArray();
+            Bind();
+            unsafe
+            {
+                fixed (void* i = &Indices[0])
+                {
+                    GraphicsInstance.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(Indices.Length * sizeof(uint)), i, BufferUsageARB.StaticDraw);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Binds the buffer.
+        /// </summary>
+        public void Bind()
+        {
+            GraphicsInstance.BindBuffer(BufferTargetARB.ElementArrayBuffer, ID);
+        }
+
+        /// <summary>
+        /// Unbinds the buffer.
+        /// </summary>
+        public void Unbind()
+        {
+            GraphicsInstance.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
+        }
+
+        /// <summary>
+        /// Deletes the IBO. Use this for disposing/cleaning up resources.
+        /// </summary>
+        public void Delete()
+        {
+            GraphicsInstance.DeleteBuffer(ID);
+        }
+
+        ~IBO()
+        {
+            Delete();
+        }
+    }
+}
