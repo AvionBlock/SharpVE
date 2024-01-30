7819d353335409be7d9331e6f8925c164d77da61
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-01-30 17:18:47 +1100

    Documentation.

--- SharpVE.Core/Interfaces/Chunks/ISubChunkData.cs
+++ SharpVE.Core/Interfaces/Chunks/ISubChunkData.cs
@@ -5,9 +5,36 @@ namespace SharpVE.Core.Interfaces.Chunks
 {
     public interface ISubChunkData
     {
+        /// <summary>
+        /// Gets a blockstate from the subchunk.
+        /// </summary>
+        /// <param name="localX">The local X coordinate inside the subchunk.</param>
+        /// <param name="localY">The local Y coordinate inside the subchunk.</param>
+        /// <param name="localZ">The local Z coordinate inside the subchunk.</param>
+        /// <returns>The blockstate.</returns>
         BlockState? GetBlock(int localX, int localY, int localZ);
+
+        /// <summary>
+        /// Gets a blockstate from the subchunk.
+        /// </summary>
+        /// <param name="localPos">The local X,Y,Z coordinate inside the subchunk.</param>
+        /// <returns>The blockstate.</returns>
         BlockState? GetBlock(Vector3D<int> localPos);
+
+        /// <summary>
+        /// Sets a blockstate into the subchunk.
+        /// </summary>
+        /// <param name="localX">The local X coordinate inside the subchunk.</param>
+        /// <param name="localY">The local Y coordinate inside the subchunk.</param>
+        /// <param name="localZ">The local Z coordinate inside the subchunk.</param>
+        /// <param name="block">The blockstate to set.</param>
         void SetBlock(int localX, int localY, int localZ, BlockState block);
+
+        /// <summary>
+        /// Sets a blockstate into the subchunk.
+        /// </summary>
+        /// <param name="localPos">The local X,Y,Z coordinate inside the subchunk.</param>
+        /// <param name="block">The blockstate to set.</param>
         void SetBlock(Vector3D<int> localPos, BlockState block);
     }
 }
