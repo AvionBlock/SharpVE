9f51ffc0e08f1efeaea746f48e6cbff51c9066c5
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-02-03 12:05:43 +1100

    Bit of cleanup.

---
+++ SharpVE.Core/Interfaces/Graphics/IShader.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpVE.Core.Interfaces.Graphics
+{
+    public interface IShader
+    {
+        /// <summary>
+        /// ID of the shader.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Uses the shader before drawing to screen.
+        /// </summary>
+        public void Use();
+
+        /// <summary>
+        /// Deletes/Disposes the shader.
+        /// </summary>
+        public void Delete();
+    }
+}
