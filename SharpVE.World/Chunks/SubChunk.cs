a9764547483cc261e2ff87ba67a11fec67c79caf
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-02-10 17:52:46 +1100

    New dedicated block storage.

--- SharpVE.World/Chunks/SubChunk.cs
+++ SharpVE.World/Chunks/SubChunk.cs
@@ -1,8 +1,7 @@
 ﻿using SharpVE.Core.Blocks;
 using SharpVE.Core.Interfaces.Chunks;
+using SharpVE.World.Storage;
 using Silk.NET.Maths;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SharpVE.World.Chunks
 {
@@ -10,32 +9,51 @@ namespace SharpVE.World.Chunks
     {
         public readonly sbyte YLevel;
 
-        private readonly Dictionary<ushort, BlockState> BlockStates;
+        private readonly BlockStorage BlockStates;
         private ChunkColumn ChunkColumn;
         private ISubChunkData Data;
 
         public SubChunk(ChunkColumn ChunkColumn, sbyte yLevel)
         {
             this.ChunkColumn = ChunkColumn;
-            BlockStates = new Dictionary<ushort, BlockState>();
+            BlockStates = new BlockStorage();
             Data = new SingleBlockSubChunkData(this);
             YLevel = yLevel;
         }
 
-        #region BlockSetters
         public void SetBlock(int localX, int localY, int localZ, BlockState block)
         {
             if (Data is SingleBlockSubChunkData)
             {
-                BlockStates.TryGetValue(Data.GetBlock(0, 0, 0), out var blockState);
+                var blockState = BlockStates.TryGetBlockState(Data.GetBlock(0, 0, 0));
                 if (blockState?.Equals(block) ?? false) return;
 
                 Data = new SubChunkData(this);
-                Data.SetBlock(localX, localY, localZ, GetOrAddBlock(block));
+                ushort blockId = 0;
+                try
+                {
+                    blockId = BlockStates.AddBlockState(block);
+                }
+                catch
+                {
+                    blockId = BlockStates.GetBlockStateId(block);
+                }
+                Data.SetBlock(localX, localY, localZ, blockId);
             }
             else if(Data is SubChunkData)
             {
-                Data.SetBlock(localX, localY, localZ, GetOrAddBlock(block));
+                ushort blockId = 0;
+                try
+                {
+                    blockId = BlockStates.AddBlockState(block);
+                }
+                catch
+                {
+                    blockId = BlockStates.GetBlockStateId(block);
+                }
+                Data.SetBlock(localX, localY, localZ, blockId);
+
+                //Do checks for converting to single block later.
             }
         }
 
@@ -46,38 +64,12 @@ namespace SharpVE.World.Chunks
 
         public BlockState? GetBlock(int localX, int localY, int localZ)
         {
-            BlockStates.TryGetValue(Data.GetBlock(0, 0, 0), out var blockState);
-            return blockState;
+            return BlockStates.TryGetBlockState(Data.GetBlock(0, 0, 0));
         }
 
         public BlockState? GetBlock(Vector3D<int> localPos)
         {
             return GetBlock(localPos.X, localPos.Y, localPos.Z);
         }
-        #endregion
-
-        #region Private Methods
-        private ushort GetLowestAvailableKey()
-        {
-            for(ushort i = 0; i < BlockStates.Count; i++)
-            {
-                if (!BlockStates.ContainsKey(i)) return i;
-            }
-            return 0;
-        }
-
-        private ushort GetOrAddBlock(BlockState block)
-        {
-            if (BlockStates.ContainsValue(block))
-            {
-                var blockInstance = BlockStates.FirstOrDefault(x => x.Value.Equals(block));
-                if (blockInstance.Value != null) return blockInstance.Key;
-            }
-
-            var blockId = GetLowestAvailableKey();
-            BlockStates.Add(blockId, block);
-            return blockId;
-        }
-        #endregion
     }
 }
---
+++ SharpVE.World/Storage/BlockStorage.cs
@@ -0,0 +1,44 @@
+﻿using SharpVE.Core.Blocks;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace SharpVE.World.Storage
+{
+    public class BlockStorage
+    {
+        public readonly Dictionary<ushort, BlockState> BlockStates;
+
+        public BlockStorage()
+        {
+            BlockStates = new Dictionary<ushort, BlockState>();
+        }
+
+        public ushort AddBlockState(BlockState block)
+        {
+            if (BlockStates.ContainsValue(block)) throw new Exception("Blockstate already exists!");
+
+            var blockId = BlockStates.Keys.Min();
+            BlockStates.Add(blockId, block);
+            return blockId;
+        }
+
+        public ushort GetBlockStateId(BlockState block)
+        {
+            var blockState = BlockStates.FirstOrDefault(x => x.Equals(block));
+            if (blockState.Value == null) throw new Exception("Blockstate does not exist!");
+            return blockState.Key;
+        }
+
+        public BlockState TryGetBlockState(ushort blockId)
+        {
+            BlockStates.TryGetValue(blockId, out var state);
+            return state;
+        }
+
+        public void RemoveBlockState(ushort blockId)
+        {
+            BlockStates.Remove(blockId);
+        }
+    }
+}
